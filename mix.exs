52347fc0753a9837fdcf87cdfdaf8300bdfc7859
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2023-10-24 20:26:51 +0200

    Configure gettext

--- mix.exs
+++ mix.exs
@@ -9,7 +9,8 @@ defmodule ShareSecret.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      gettext: [write_reference_line_numbers: false]
     ]
   end
 
