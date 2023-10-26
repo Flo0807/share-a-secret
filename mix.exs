1e96c03730057f4d72d83c56475f1688dbb5a6be
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2023-10-26 14:26:22 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ShareSecret.MixProject do
   def project do
     [
       app: :share_secret,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
