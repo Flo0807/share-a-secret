7aa28b3068f882e5cddace76e3f90612502d0459
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2023-12-28 12:42:21 +0100

    Update version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ShareSecret.MixProject do
   def project do
     [
       app: :share_secret,
-      version: "0.1.4",
+      version: "0.2.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
