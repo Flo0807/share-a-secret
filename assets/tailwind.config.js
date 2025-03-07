8071b7fdfd037d7a72dcde38e527cb862cac95f8
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-03-07 18:09:53 +0100

    Format JS files with standard

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,47 +1,47 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
-const plugin = require("tailwindcss/plugin")
-const fs = require("fs")
-const path = require("path")
+const plugin = require('tailwindcss/plugin')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = {
   content: [
-    "./js/**/*.js",
-    "../lib/*_web.ex",
-    "../lib/*_web/**/*.*ex"
+    './js/**/*.js',
+    '../lib/*_web.ex',
+    '../lib/*_web/**/*.*ex'
   ],
   daisyui: {
     themes: [
       {
         light: {
-          ...require("daisyui/src/theming/themes")["light"],
-          primary: "#2563eb",
-          warning: "#fbbf24",
-          info: "#38bdf8",
+          ...require('daisyui/src/theming/themes').light,
+          primary: '#2563eb',
+          warning: '#fbbf24',
+          info: '#38bdf8',
           'primary-content': 'white'
         },
         dark: {
-          ...require("daisyui/src/theming/themes")["dark"],
-          primary: "#2563eb",
-          warning: "#fbbf24",
-          info: "#38bdf8",
+          ...require('daisyui/src/theming/themes').dark,
+          primary: '#2563eb',
+          warning: '#fbbf24',
+          info: '#38bdf8',
           'primary-content': 'white'
         }
-      },
-    ],
+      }
+    ]
   },
   plugins: [
-    require("@tailwindcss/typography"),
-    require("daisyui"),
+    require('@tailwindcss/typography'),
+    require('daisyui'),
     // Allows prefixing tailwind classes with LiveView classes to add rules
     // only when LiveView classes are applied, for example:
     //
     //     <div class="phx-click-loading:animate-ping">
     //
-    plugin(({ addVariant }) => addVariant("phx-click-loading", [".phx-click-loading&", ".phx-click-loading &"])),
-    plugin(({ addVariant }) => addVariant("phx-submit-loading", [".phx-submit-loading&", ".phx-submit-loading &"])),
-    plugin(({ addVariant }) => addVariant("phx-change-loading", [".phx-change-loading&", ".phx-change-loading &"])),
+    plugin(({ addVariant }) => addVariant('phx-click-loading', ['.phx-click-loading&', '.phx-click-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-submit-loading', ['.phx-submit-loading&', '.phx-submit-loading &'])),
+    plugin(({ addVariant }) => addVariant('phx-change-loading', ['.phx-change-loading&', '.phx-change-loading &'])),
 
     // Embeds Heroicons (https://heroicons.com) into your app.css bundle
     // See your `CoreComponents.icon/1` for more information.
