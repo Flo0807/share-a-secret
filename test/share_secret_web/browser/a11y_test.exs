7592e9142855f4f7d94f5685dd34f077be06565f
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2025-10-26 13:49:33 +0100

    Add a11y test

---
+++ test/share_secret_web/browser/a11y_test.exs
@@ -0,0 +1,24 @@
+defmodule ShareSecretWeb.Browser.A11yTest do
+  use PhoenixTest.Playwright.Case, async: true
+  use ShareSecretWeb, :verified_routes
+
+  alias PhoenixTest.Playwright.Frame
+
+  test "index page has no accessibility violations", %{conn: conn} do
+    conn
+    |> visit(~p"/")
+    |> assert_a11y()
+  end
+
+  defp assert_a11y(session) do
+    Frame.evaluate(session.frame_id, A11yAudit.JS.axe_core())
+
+    {:ok, result} = Frame.evaluate(session.frame_id, "axe.run()")
+
+    result
+    |> A11yAudit.Results.from_json()
+    |> A11yAudit.Assertions.assert_no_violations()
+
+    session
+  end
+end
